87d59df385fa4c63936b893cef4193dca2253c16
maminez23 <amineziadi23@gmail.com>
2020-03-23 17:50:08 +0100

    lakhdhar

--- src/jigglyDnce.js
+++ src/jigglyDnce.js
@@ -19,3 +19,22 @@ makejigglyDancer.prototype.step = function () {
 };
 
 
+var makeLakhdharDancer = function(top, left, timeBetweenSteps) {
+    // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+    // so we must keep a copy of the old version of this function
+    MakeDancer.call(this,top, left, timeBetweenSteps);
+    this.$node = $('<span class="dancer2"></span>')
+    this.setPosition(top, left)
+
+};
+
+makeLakhdharDancer.prototype = Object.create(MakeDancer.prototype);
+makeLakhdharDancer.prototype.constructor = makeLakhdharDancer;
+
+
+
+makeLakhdharDancer.prototype.step = function () {
+
+    MakeDancer.prototype.step.call(this, this.timeBetweenSteps);
+    this.$node.toggle();
+};
